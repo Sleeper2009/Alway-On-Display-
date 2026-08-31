e376f02fad5c69894b8a568b1c6207434b277fcc
Sleeper2009 <thao10101010@icloud.com>
2026-08-31 21:44:05 +0700

    Implement AODTweakPrefsListController with specifiers

---
+++ AODTweakPrefs/AODTweakPrefsListController.m
@@ -0,0 +1,12 @@
+#import "AODTweakPrefsListController.h"
+
+@implementation AODTweakPrefsListController
+
+- (NSArray *)specifiers {
+    if (!_specifiers) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    }
+    return _specifiers;
+}
+
+@end
